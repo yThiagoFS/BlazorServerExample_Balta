e4be87ea36a221aa61ad494e074cdf6a0091f671
Thiago Silva <thi.ferreira.silva03@gmail.com>
2023-10-08 18:13:16 -0300

    added input select with categories

--- BlazorServerCrud/Data/AppDbContext.cs
+++ BlazorServerCrud/Data/AppDbContext.cs
@@ -8,7 +8,7 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> opts) : base(opts)
     {}
 
-    public DbSet<Product> Products{ get; set; }
+    public DbSet<Product> Products { get; set; }
 
-    public DbSet<Category> Categories{ get; set; }
+    public DbSet<Category> Categories { get; set; }
 }
